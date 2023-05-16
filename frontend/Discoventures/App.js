d7079f5dce9713e053566e9c1c541ccb3182c12a
LouisJeudy <louisjeudy4@gmail.com>
2023-05-16 10:41:22 +0200

    logout & begin of cardmap

--- frontend/Discoventures/App.js
+++ frontend/Discoventures/App.js
@@ -3,10 +3,18 @@ import * as React from 'react';
 import { Provider } from 'react-redux';
 import store from './src/app/store/store';
 import AppHome from './App_home';
+<<<<<<< HEAD
 import { useFonts } from 'expo-font';
 
 export default function App() {
 
+=======
+import Profile from './src/components/Profile';
+import { useFonts } from 'expo-font';
+import MapCard from './src/components/MapCard';
+
+export default function App() {
+>>>>>>> be9ea4d (logout & begin of cardmap)
   const [fontsLoaded] = useFonts({
     'Raleway-Bold': require('./assets/fonts/Raleway/Raleway-Bold.ttf'),
     'Raleway-SemiBold': require('./assets/fonts/Raleway/Raleway-SemiBold.ttf'),
@@ -17,10 +25,20 @@ export default function App() {
   if (!fontsLoaded) {
     return null;
   }
+<<<<<<< HEAD
     return (
       <Provider store={store}>
         <AppHome/>
       </Provider>
     );
+=======
+
+    //const { signIn } = React.useContext(AuthContext);
+    return (
+    <Provider store={store}>
+      <MapCard/>
+    </Provider>
+      );
+>>>>>>> be9ea4d (logout & begin of cardmap)
   }
  
